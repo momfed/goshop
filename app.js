537b62d8bb67ca46978ae3d40e2d8f524062b970
momfed <arbihysko@gmail.com>
2017-01-28 14:35:18 +0100

    added node express and nodemon and made some changes at the node app

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	var readstream = fs.createReadStream(__dirname + '/home.html', 'utf8');
-	readstream.pipe(res);
+	console.log('The request made to: ' + req.url);
+	if (req.url === '/home' || req.url === '/'){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/home.html').pipe(res);
+	} else if(req.url === '/contact'){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/contact.html').pipe(res);
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 });
 
 server.listen(3000, 'localhost');

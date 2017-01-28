e990a9b38e1928edd3ce4c93f57a8b7b37a9dcb3
momfed <arbihysko@gmail.com>
2017-01-28 02:10:34 +0100

    added a server

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('yaayyy u got me');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var readstream = fs.createReadStream(__dirname + '/home.html', 'utf8');
+	readstream.pipe(res);
 });
 
 server.listen(3000, 'localhost');
